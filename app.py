c728e063bc7dbeb73ce86392fe64f29e9df7d9f7
Tamilmani N <ntamilmanin@gmail.com>
2026-01-08 11:38:44 +0000

    Final auto-deploy-test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Auto Deployed via GitHub Actions 🚀"
+    return "CI/CD Auto Deploy - Build Successful 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
